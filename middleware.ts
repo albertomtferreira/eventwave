1290a5326e7ba33fa44b855660f3b7c7ea36a630
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2024-09-26 11:57:00 +0000

    codesandbox

--- middleware.ts
+++ middleware.ts
@@ -6,25 +6,23 @@ export default clerkMiddleware({
     "/events/:id",
     "/api/webhook/clerk",
     "/api/webhook/stripe",
-    "/api/uploadthing"
+    "/api/uploadthing",
   ],
   ignoredRoutes: [
     "/api/webhook/clerk",
     "/api/webhook/stripe",
-    "/api/uploadthing"
+    "/api/uploadthing",
   ],
-
 });
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
-
 
 // import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
